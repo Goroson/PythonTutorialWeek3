8a2115f0abf7e9952fbbb9bcca69fc0ce1a5bb7a
goroson <goroson2@gmail.com>
2018-02-25 01:47:11 +0300

    task v1.0 completed

--- venv/solutions/solution 2.py
+++ venv/solutions/solution 2.py
@@ -1,28 +1,84 @@
+import os
+import csv
+
 class CarBase:
     def __init__(self, brand, photo_file_name, carrying):
-        self.brand = brand
-        self.photo_file_name = photo_file_name
-        self.carrying = carrying
+        try:
+            if not brand:
+                raise ValueError
+            self.brand = brand
+            self.photo_file_name = photo_file_name
+            self.carrying = float(carrying)
+            self.get_photo_file_ext()
+        except:
+            raise
+
+    def get_photo_file_ext(self):
+        res = os.path.splitext(self.photo_file_name)
+        if res[1] == '':
+            raise ValueError
+        return res[1]
+
+
 
 
 class Car(CarBase):
     def __init__(self, brand, photo_file_name, carrying, passenger_seats_count):
         super().__init__(brand,photo_file_name, carrying)
-        self.passenger_seats_count = passenger_seats_count
+        try:
+            self.passenger_seats_count = int(passenger_seats_count)
+        except:
+            raise
+
 
 
 class Truck(CarBase):
     def __init__(self, brand, photo_file_name, carrying, body_whl):
         super().__init__(brand, photo_file_name, carrying)
-        self.body_whl = body_whl
+        try:
+            if not body_whl or None:
+                body_whl = '0x0x0'
+
+            self.body_whl = body_whl.split('x')
+            self.body_length = float(self.body_whl[0]) or 0.0
+            self.body_width = float(self.body_whl[1]) or 0.0
+            self.body_height = float(self.body_whl[2]) or 0.0
+            del self.body_whl
+        except:
+            raise
+
+    def get_body_volume(self):
+        return (self.body_length * self.body_height * self.body_width)
+
 
 
 class SpecMachine(CarBase):
     def __init__(self, brand, photo_file_name, carrying, extra):
         super().__init__(brand, photo_file_name, carrying)
+        if not extra:
+            raise ValueError
         self.extra = extra
+
+
+
 
 
 def get_car_list(csv_filename):
     car_list = []
+    with open(csv_filename) as csv_fd:
+        reader = csv.reader(csv_fd, delimiter=';')
+        next(reader)  # пропускаем заголовок
+        for row in reader:
+            if row.__len__() != 7:
+                continue
+            try:
+                if row[0] == 'car':
+                    car_list.append(Car(row[1], row[3], row[5], row[2]))
+                elif row[0] == 'truck':
+                    car_list.append(Truck(row[1], row[3], row[5], row[4]))
+                elif row[0] == 'spec_machine':
+                    car_list.append(SpecMachine(row[1], row[3], row[5], row[6]))
+            except:
+                continue
+
     return car_list
