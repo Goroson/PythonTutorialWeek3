75d92ce2a368aa39ade6d86026501032bdae4d2c
goroson <goroson2@gmail.com>
2018-02-25 03:07:05 +0300

    tsk v 1.0 fixed v6

--- venv/solutions/solution 2.py
+++ venv/solutions/solution 2.py
@@ -2,8 +2,9 @@ import os
 import csv
 
 class CarBase:
-    def __init__(self, brand, photo_file_name, carrying):
+    def __init__(self, car_type, brand, photo_file_name, carrying):
         try:
+            self.car_type = car_type
             self.brand = brand
             self.photo_file_name = photo_file_name
             self.carrying = float(carrying)
@@ -21,19 +22,19 @@ class CarBase:
 
 
 class Car(CarBase):
-    def __init__(self, brand, photo_file_name, carrying, passenger_seats_count):
-        super().__init__(brand,photo_file_name, carrying)
+    def __init__(self, car_type, brand, photo_file_name, carrying, passenger_seats_count):
+        super().__init__(car_type, brand,photo_file_name, carrying)
         try:
             self.passenger_seats_count = int(passenger_seats_count)
         except:
             raise
 
     def __str__(self):
-        return('car with {0} brand, {1} photo file name, {2} carrying weight, {3} passengers'.format(self.brand, self.photo_file_name, self.carrying, self.passenger_seats_count))
+        return('{4} with {0} brand, {1} photo file name, {2} carrying weight, {3} passengers'.format(self.brand, self.photo_file_name, self.carrying, self.passenger_seats_count, self.car_type))
 
 class Truck(CarBase):
-    def __init__(self, brand, photo_file_name, carrying, body_whl):
-        super().__init__(brand, photo_file_name, carrying)
+    def __init__(self, car_type, brand, photo_file_name, carrying, body_whl):
+        super().__init__(car_type, brand, photo_file_name, carrying)
         try:
             body_whl = body_whl.split('x')
             if body_whl.__len__() != 3:
@@ -48,18 +49,18 @@ class Truck(CarBase):
         return (self.body_length * self.body_height * self.body_width)
 
     def __str__(self):
-        return('car with {0} brand, {1} photo file name, {2} carrying weight, {3} volume'.format(self.brand, self.photo_file_name, self.carrying, self.get_body_volume()))
+        return('{4} with {0} brand, {1} photo file name, {2} carrying weight, {3} volume'.format(self.brand, self.photo_file_name, self.carrying, self.get_body_volume(), self.car_type))
 
 
 class SpecMachine(CarBase):
-    def __init__(self, brand, photo_file_name, carrying, extra):
-        super().__init__(brand, photo_file_name, carrying)
+    def __init__(self, car_type, brand, photo_file_name, carrying, extra):
+        super().__init__(car_type, brand, photo_file_name, carrying)
         if not extra:
             raise ValueError
         self.extra = extra
 
     def __str__(self):
-        return ('car with {0} brand, {1} photo file name, {2} carrying weight, {3}'.format(self.brand, self.photo_file_name, self.carrying, self.extra))
+        return ('{4} with {0} brand, {1} photo file name, {2} carrying weight, {3}'.format(self.brand, self.photo_file_name, self.carrying, self.extra, self.car_type))
 
 
 
@@ -75,13 +76,14 @@ def get_car_list(csv_filename):
                 if row.__len__() != 7:
                     continue
                 if row[0] == 'car':
-                    car_list.append(Car(row[1], row[3], row[5], row[2]))
+                    car_list.append(Car(row[0], row[1], row[3], row[5], row[2]))
                 elif row[0] == 'truck':
-                    car_list.append(Truck(row[1], row[3], row[5], row[4]))
+                    car_list.append(Truck(row[0], row[1], row[3], row[5], row[4]))
                 elif row[0] == 'spec_machine':
-                    car_list.append(SpecMachine(row[1], row[3], row[5], row[6]))
+                    car_list.append(SpecMachine(row[0], row[1], row[3], row[5], row[6]))
             except:
                 continue
+            #print(row)
     return car_list
 
-#cars = get_car_list('/home/goroson/Downloads/cars.csv')
+#cars = get_car_list('/home/goroson/Downloads/csv.csv')
